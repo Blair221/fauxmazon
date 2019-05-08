73e3f55a317c14e4ff24be1785f7ca0c979f3283
Blair Brownlie <bbrownli@asu.edu>
2019-05-07 22:55:19 -0700

    can call to the db and display the table, just need to sort out the updating of the db from the node app

---
+++ fauxmazon.js
@@ -0,0 +1,60 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  port: 3306,
+  password: "Stev!eg33",
+  database: "fauxmazon_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+connection.query("select * from products", function(error, results) {
+  if (error) {
+    console.log("there was an error making that query");
+  }
+
+  console.table(results);
+  inquirer
+    .prompt([
+      {
+        name: "product_id",
+        type: "number",
+        message: "What is the id of the product you would like to order?",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          } else {
+            console.log('\nmake sure to input a number!');     
+            return false;
+          }
+        }
+      },
+      {
+        name: "product_quant",
+        type: "number",
+        message: "How many would you like to order?",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      }
+    ])
+    .then(function(answers) {
+      console.log(`The product you want to order is id: ${answers.product_id}`);
+      console.log(`and you want to order ${answers.product_quant} of them`);
+      console.table(results);
+    });
+});
